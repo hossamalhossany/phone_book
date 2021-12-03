cc0ce5968957817913fc067532a401cec39db88e
hossamalhossany <hossam.2030@gmail.com>
2021-12-04 01:30:30 +0200

    commit 11

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'ph_book.apps.PhBookConfig'
-    'ph_book.apps.AppConfig'
+    'ph_book.apps.PhBookConfig'
+    # 'ph_book.apps.AppConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
