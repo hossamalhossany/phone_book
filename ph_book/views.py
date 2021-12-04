62205a93552cefb0db4264e1b5d75d5b12e9f466
hossam <hossam@hossam.com>
2021-12-04 16:02:53 +0200

    commit 15

--- ph_book/views.py
+++ ph_book/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("phone book 4-12-2021")
-    # this from work for test git and git hub
+    # this from work for test git and git hub 4/12/2021 4 pm
 
 
