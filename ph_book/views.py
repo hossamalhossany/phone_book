ab3647540e8a8e5a5d35db94e68ec9b6400ee313
hossam <hossam@hossam.com>
2021-12-06 16:32:06 +0200

    commit 6-12-2021 4:32 pm

--- ph_book/views.py
+++ ph_book/views.py
@@ -1,13 +1,9 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
-
-
 def index(request):
     return_value(request)
     return HttpResponse(return_value(request))
-    # this from work for test git and git hub 4/12/2021 4 pm
-
 
 def return_value(request):
     with connection.cursor() as cursor:
