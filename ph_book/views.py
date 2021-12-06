24827873e8a3874c0ae0560f5c45d8d3960875ac
hossam <hossam@hossam.com>
2021-12-06 16:30:49 +0200

    commit 6-12-2021 4:30 pm

--- ph_book/views.py
+++ ph_book/views.py
@@ -1,9 +1,19 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 
 
 def index(request):
-    return HttpResponse("phone book 4-12-2021")
+    return_value(request)
+    return HttpResponse(return_value(request))
     # this from work for test git and git hub 4/12/2021 4 pm
 
 
+def return_value(request):
+    with connection.cursor() as cursor:
+        sql = ('''
+            select * from phonebook.ph_book_test1
+        ''')
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    return rows
