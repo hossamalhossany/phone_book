1df95bcfaaadea203d1344ce4c3f3c61980eafbd
hossam <hossam@hossam.com>
2021-12-07 14:16:43 +0200

    commit 7-12-2021 2:16 pm

--- ph_book/views.py
+++ ph_book/views.py
@@ -4,21 +4,22 @@ from django.db import connection
 
 
 def index(request):
-    return_value(request)
-    return HttpResponse(return_value(request))
-
-
-def return_value(request):
-    with connection.cursor() as cursor:
-        sql = ('''
-            insert into phonebook.ph_book_test1 (first_name, last_name) VALUES 
-            ("ahmed","ali")
-        ''')
-        cursor.execute(sql)
-
-        sql = ('''
-            select * from phonebook.ph_book_test1
-        ''')
-        cursor.execute(sql)
-        rows = cursor.fetchall()
-    return rows
+    return HttpResponse("ph_book/index.html")
+#     return_value(request)
+#     return HttpResponse(return_value(request))
+#
+#
+# def return_value(request):
+#     with connection.cursor() as cursor:
+#         sql = ('''
+#             insert into phonebook.ph_book_test1 (first_name, last_name) VALUES
+#             ("ahmed","ali")
+#         ''')
+#         cursor.execute(sql)
+#
+#         sql = ('''
+#             select * from phonebook.ph_book_test1
+#         ''')
+#         cursor.execute(sql)
+#         rows = cursor.fetchall()
+#     return rows
