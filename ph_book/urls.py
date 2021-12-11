9b2324a0bbbbf7d369b791b9035d605f6befd653
hossam <hossam@hossam.com>
2021-12-11 15:57:26 +0200

    commit 11-12-2021 4:00 pm

--- ph_book/urls.py
+++ ph_book/urls.py
@@ -1,8 +1,10 @@
+from django.http import request
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("put_data_to_databasa", views.put_data_to_databasa, name='put_data_to_databasa')
 ]
 
 
--- ph_book/views.py
+++ ph_book/views.py
@@ -1,18 +1,27 @@
 from django.db import connection
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 from .forms import contact_form
-
+from .models import test1
 
 def index(request):
     html_page = "ph_book/index.html"
     return render(request, html_page, {'contact_form': contact_form})
 
-def put_data_to_databasa():
-  with connection.cursor() as cursor:
-        sql = ('''
-            insert into phonebook.ph_book_test1 (first_name, last_name) VALUES
-            ("ahmed","ali")
-        ''')
-        cursor.execute(sql)
+
+def put_data_to_databasa(request):
+    test1 = test1
+    test1.first_name = request.POST.get('first_name')
+    test1.last_name = request.POST.get('last_name')
+    test1.save()
+    # first_name = request.GET['first_name']
+    # last_name = request['last_name']
+    # with connection.cursor() as cursor:
+        # sql = ('''
+        #     insert into phonebook.ph_book_test1 (first_name , last_name) VALUES
+        #     (request.POST.get('first_name'),request.POST.get('last_name'))
+        #
+        # ''')
+        # return sql
+        # cursor.execute(sql)
