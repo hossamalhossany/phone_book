d886652209175da002c047e1267bf7488c53a33b
hossam <hossam@hossam.com>
2021-12-11 11:22:21 +0200

    commit 11-12-2021 11:22 am

--- ph_book/models.py
+++ ph_book/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class test1(models.Model):
     first_name = models.CharField(max_length=100)
+
     last_name = models.CharField(max_length=50)
     #THIS 6-12-2021 test22
