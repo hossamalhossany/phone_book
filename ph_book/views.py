e7b88f9bcbec183f4de300e6aef279b98ff8a14c
hossam <hossam@hossam.com>
2021-12-11 14:11:19 +0200

    commit 11-12-2021 2:00 pm

--- ph_book/views.py
+++ ph_book/views.py
@@ -1,6 +1,6 @@
+from django.db import connection
 from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.db import connection
+
 
 from .forms import contact_form
 
@@ -8,22 +8,11 @@ from .forms import contact_form
 def index(request):
     html_page = "ph_book/index.html"
     return render(request, html_page, {'contact_form': contact_form})
-    # return HttpResponse("ph_book/index.html")
-#     return_value(request)
-#     return HttpResponse(return_value(request))
-#
-#
-# def return_value(request):
-#     with connection.cursor() as cursor:
-#         sql = ('''
-#             insert into phonebook.ph_book_test1 (first_name, last_name) VALUES
-#             ("ahmed","ali")
-#         ''')
-#         cursor.execute(sql)
-#
-#         sql = ('''
-#             select * from phonebook.ph_book_test1
-#         ''')
-#         cursor.execute(sql)
-#         rows = cursor.fetchall()
-#     return rows
+
+def put_data_to_databasa():
+  with connection.cursor() as cursor:
+        sql = ('''
+            insert into phonebook.ph_book_test1 (first_name, last_name) VALUES
+            ("ahmed","ali")
+        ''')
+        cursor.execute(sql)
